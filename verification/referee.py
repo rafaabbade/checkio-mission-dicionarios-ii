5f2b954ead51a30c117f732bd520e22a8a9f88b7
rafaabbade <82163300+rafaabbade@users.noreply.github.com>
2024-04-29 16:50:22 -0300

    Update referee.py

--- verification/referee.py
+++ verification/referee.py
@@ -38,7 +38,7 @@ api.add_listener(
     CheckiOReferee(
         tests=TESTS,
         function_name={
-            "python": "adicionar_genero",
+            "python": "explorar_dicionario",
             "js": "countConsecutiveSummers"
         },
         cover_code={
